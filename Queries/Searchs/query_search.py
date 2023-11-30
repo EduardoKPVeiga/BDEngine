c90210a45a1f6408e7a39b4261fa68f3f78e0cb2
unknown <epontesveiga@gmail.com>
2023-11-30 20:33:34 -0300

    Simple select update

--- Queries/Searchs/query_search.py
+++ Queries/Searchs/query_search.py
@@ -9,7 +9,10 @@ data_select = []
 columns = []
 
 def query(command):
+    global data_select
     global columns
+    
+    data_select = []
 
     if command.find(" " + cmds.FROM + " ") == -1:
         print("ERROR: incorrect setence")
@@ -17,16 +20,27 @@ def query(command):
     
     list_words = command.split()
 
-    if list_words[1] == "*" or list_words[1] == "(*)":
-        table_data = BDxml_root.find('table_data')
-        for row in table_data.findall('row'):
-            fields = []
-            for field in row.findall('field'):
-                fields.append(field.text)
-            data_select.append(fields)
+    table_name = ""
+    getTableName = False
+    for word in list_words:
+        if word == cmds.FROM:
+            getTableName = True
+            continue
+        if getTableName:
+            table_name = word
+            break
 
-        print(np.matrix(data_select))
-        return True
+    if list_words[1] == "*" or list_words[1] == "(*)":
+        for table_data in BDxml_root.findall('table_data'):
+            if table_data.get('name') == table_name:
+                for row in table_data.findall('row'):
+                    fields = []
+                    for field in row.findall('field'):
+                        fields.append(field.text)
+                    data_select.append(fields)
+
+                print(np.matrix(data_select))
+                return True
 
     if list_words[1].find(")") != -1:
         if list_words[1].find("(") == -1:
